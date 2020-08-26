30670e84d25ef833f094858ecfe1d5f217a7743c
Varun Pusarla <varunpusarla@gmail.com>
2020-08-26 10:51:25 +0530

    Added styling

--- textutils/views.py
+++ textutils/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request, 'index.html')
+    return render(request, 'index2.html')
 
 def analyze(request):
     #Get the text
